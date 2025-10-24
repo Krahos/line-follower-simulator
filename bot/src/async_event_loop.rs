72c5bd3c3b7cae1418e10053a75bd74184254c79
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2025-10-24 14:35:09 +0200

    Add async utilities

--- bot/src/async_event_loop.rs
+++ bot/src/async_event_loop.rs
@@ -1,7 +1,10 @@
 use std::{
+    cell::Cell,
     pin::Pin,
     task::{Context, Poll},
 };
+
+use pin_project_lite::pin_project;
 
 use crate::line_follower_robot::devices::{
     DeviceValue, FutureHandle, PollOperationStatus, device_poll, forget_handle, poll_loop,
@@ -76,5 +79,208 @@ impl Future for FutureValue {
 impl Drop for FutureValue {
     fn drop(&mut self) {
         forget_handle(self.handle);
+    }
+}
+
+/// This acts like a single-value channel that only retains the most recent value
+pub struct ValueWatcher<T> {
+    counter: Cell<usize>,
+    value: Cell<T>,
+}
+
+impl<T: Copy + Default> ValueWatcher<T> {
+    /// Create the channel
+    pub fn new() -> Self {
+        ValueWatcher {
+            counter: Cell::new(0),
+            value: Cell::new(T::default()),
+        }
+    }
+
+    /// Get the current value (returns immediately)
+    pub fn get(&self) -> T {
+        self.value.get()
+    }
+
+    /// Update the current value
+    pub fn update(&self, value: T) {
+        self.counter.set(self.counter.get() + 1);
+        self.value.set(value);
+    }
+
+    /// Wait for a new value (set using `update`)
+    pub fn next<'a>(&'a self) -> NextValue<'a, T> {
+        NextValue {
+            sender: self,
+            counter: self.counter.get() + 1,
+        }
+    }
+
+    /// Get a stream of new values
+    pub fn stream<'a>(&'a self) -> ValueStream<'a, T> {
+        ValueStream {
+            sender: self,
+            counter: self.counter.get(),
+        }
+    }
+}
+
+/// A future value that resolves when its channel is updated
+#[must_use = "futures do nothing unless you `.await` or poll them"]
+pub struct NextValue<'a, T> {
+    sender: &'a ValueWatcher<T>,
+    counter: usize,
+}
+
+impl<'a, TO> NextValue<'a, TO> {
+    /// Map the value to a new type
+    pub fn map<TM>(
+        self,
+        mapper: &'a impl Fn(TO) -> TM,
+    ) -> MappedValue<'a, Self, impl Fn(TO) -> TM> {
+        MappedValue {
+            original: self,
+            mapper,
+        }
+    }
+
+    pub fn filter(
+        self,
+        filter: &'a impl Fn(&TO) -> bool,
+    ) -> FilteredValue<'a, Self, impl Fn(&TO) -> bool> {
+        FilteredValue {
+            original: self,
+            filter,
+        }
+    }
+}
+
+impl<T: Copy + Default> Future for NextValue<'_, T> {
+    type Output = T;
+
+    fn poll(
+        self: core::pin::Pin<&mut Self>,
+        _cx: &mut core::task::Context,
+    ) -> core::task::Poll<Self::Output> {
+        if self.sender.counter.get() >= self.counter {
+            core::task::Poll::Ready(self.sender.get())
+        } else {
+            core::task::Poll::Pending
+        }
+    }
+}
+
+pin_project! {
+    /// A mapped future value
+    #[must_use = "futures do nothing unless you `.await` or poll them"]
+    pub struct MappedValue<'a, FUTURE, MAPPER> {
+        #[pin]
+        original: FUTURE,
+        mapper: &'a MAPPER,
+    }
+}
+
+impl<'a, FUTURE: Future<Output = TO> + 'a, TO, TM, MAPPER: Fn(TO) -> TM>
+    MappedValue<'a, FUTURE, MAPPER>
+{
+    pub fn new(original: FUTURE, mapper: &'a MAPPER) -> Self {
+        MappedValue { original, mapper }
+    }
+
+    /// Filter the value with a predicate
+    pub fn filter<FILTER>(
+        self,
+        filter: &'a impl Fn(TO) -> bool,
+    ) -> FilteredValue<'a, Self, impl Fn(TO) -> bool> {
+        FilteredValue {
+            original: self,
+            filter,
+        }
+    }
+}
+
+impl<'a, FUTURE: Future<Output = TO> + 'a, TO, TM, MAPPER: Fn(TO) -> TM> Future
+    for MappedValue<'a, FUTURE, MAPPER>
+{
+    type Output = TM;
+
+    fn poll(
+        self: core::pin::Pin<&mut Self>,
+        cx: &mut core::task::Context,
+    ) -> core::task::Poll<Self::Output> {
+        let this = self.project();
+        match this.original.poll(cx) {
+            core::task::Poll::Ready(value) => core::task::Poll::Ready((this.mapper)(value)),
+            core::task::Poll::Pending => core::task::Poll::Pending,
+        }
+    }
+}
+
+pin_project! {
+    #[must_use = "futures do nothing unless you `.await` or poll them"]
+    pub struct FilteredValue<'a, FUTURE, FILTER> {
+        #[pin]
+        original: FUTURE,
+        filter: &'a FILTER,
+    }
+}
+
+impl<'a, FUTURE: Future<Output = TO> + 'a, TO, FILTER: Fn(&TO) -> bool>
+    FilteredValue<'a, FUTURE, FILTER>
+{
+    pub fn new(original: FUTURE, filter: &'a FILTER) -> Self {
+        FilteredValue { original, filter }
+    }
+
+    /// Map the filtered value to a new type
+    pub fn map<TM>(
+        self,
+        mapper: &'a impl Fn(TO) -> TM,
+    ) -> MappedValue<'a, Self, impl Fn(TO) -> TM> {
+        MappedValue {
+            original: self,
+            mapper,
+        }
+    }
+}
+
+impl<'a, FUTURE: Future<Output = TO> + 'a, TO, FILTER: Fn(&TO) -> bool> Future
+    for FilteredValue<'a, FUTURE, FILTER>
+{
+    type Output = FUTURE::Output;
+
+    fn poll(
+        self: core::pin::Pin<&mut Self>,
+        cx: &mut core::task::Context,
+    ) -> core::task::Poll<Self::Output> {
+        let this = self.project();
+        match this.original.poll(cx) {
+            core::task::Poll::Ready(value) => {
+                if (this.filter)(&value) {
+                    core::task::Poll::Ready(value)
+                } else {
+                    core::task::Poll::Pending
+                }
+            }
+            core::task::Poll::Pending => core::task::Poll::Pending,
+        }
+    }
+}
+
+/// An asynchronous stream of values
+pub struct ValueStream<'a, T: Copy + Default> {
+    sender: &'a ValueWatcher<T>,
+    counter: usize,
+}
+
+impl<T: Copy + Default> ValueStream<'_, T> {
+    /// Get the next value from the stream
+    pub fn next<'a>(&'a mut self) -> NextValue<'a, T> {
+        let counter = self.sender.counter.get().max(self.counter);
+        self.counter = counter + 1;
+        NextValue {
+            sender: self.sender,
+            counter,
+        }
     }
 }
