301e7029c68dc5b0cf62efe55d950c2c0b1a85ce
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2025-10-29 21:45:32 +0100

    sim: use plain transform for IMU data

--- sim/sim/src/bot/sensors/imu.rs
+++ sim/sim/src/bot/sensors/imu.rs
@@ -5,12 +5,12 @@ use super::bot_position::BotPositionDetector;
 use execution_data::{GyroData, ImuFusedData, SensorsData};
 
 pub fn compute_imu_data(
-    bot_query: Query<(&GlobalTransform, &Velocity), With<BotPositionDetector>>,
+    bot_query: Query<(&Transform, &Velocity), With<BotPositionDetector>>,
     mut sensors_data: ResMut<SensorsData>,
 ) {
     let (transform, velocity) = bot_query.single().unwrap();
 
-    let body_rot = Vec3::from(transform.rotation().to_euler(EulerRot::XYZ));
+    let body_rot = Vec3::from(transform.rotation.to_euler(EulerRot::XYZ));
     sensors_data.gyro = GyroData::from(velocity.angvel * body_rot);
 
     sensors_data.imu_fused = ImuFusedData::from(body_rot);
