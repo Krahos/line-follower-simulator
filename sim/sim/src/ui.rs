7feb21d71d1d92c296143c8030500b2516a3448c
Mikle <m.michele.2003@gmail.com>
2025-10-31 13:56:35 +0100

    ui: add help.md

--- sim/sim/src/ui.rs
+++ sim/sim/src/ui.rs
@@ -1,4 +1,5 @@
 use egui_commonmark::*;
+
 use std::{
     f32::consts::{FRAC_PI_4, PI},
     path::PathBuf,
@@ -329,22 +330,8 @@ pub fn help_dialog(ui: &mut Ui, help_open: &mut bool, base_text_size: f32) {
     let close = if *help_open {
         let modal = Modal::new(Id::new("Modal Error")).show(ui.ctx(), |ui| {
             ui.vertical_centered(|ui| {
-                let markdown = r"# Hello world
-
-* A list
-* [ ] Checkbox
-";
-
                 let mut cache = CommonMarkCache::default();
-                CommonMarkViewer::new().show(ui, &mut cache, markdown);
-
-                // rl(ui, "Help", base_text_size * 3.0);
-
-                // ui.add_space(8.0);
-
-                // rl(ui, "HELP!", base_text_size * 1.5);
-
-                // ui.add_space(8.0);
+                commonmark_str!(ui, &mut cache, "help.md");
 
                 if icon_button(ui, ICON_CHECK, base_text_size * 4.0).clicked() {
                     ui.close();
